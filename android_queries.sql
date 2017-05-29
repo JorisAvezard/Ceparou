7d5fbf98b39948fba7d58e48861995c69d98d8c6
JorisAvezard <avezard.joris@gmail.com>
2017-05-29 13:50:42 +0200

    deleted:    Cours.zip 	deleted:    TD_Correction.zip 	deleted:    android_data_examples.sql 	deleted:    android_queries.sql 	deleted:    android_tables.sql 	deleted:    test.txt

--- android_queries.sql
+++
@@ -1,7 +0,0 @@
--- Return name of the rooms where the user's already gone
-
-SELECT DISTINCT pl.name_place
-FROM android.places AS pl, android.buildings AS b, android.paths AS pa
-WHERE pa.user_id=1 AND b.id_building=pa.building_id AND b.id_building=pl.building_id
-    AND ST_Within(pa.coordinates, pl.area)='true';
-	
